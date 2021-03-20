f1994fbad71c2f9c431b6c7c71e6072fbb6f6f3c
neowiz31 <neowiz23@gmail.com>
2021-03-21 00:53:23 +0900

    投稿画面のデザイン実装

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,5 +3,19 @@ class GamesController < ApplicationController
     @games = Game.all
   end
 
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
 
 end
