a0c6791780a70df02ae40ace2eb92952ba403899
neowiz31 <neowiz23@gmail.com>
2021-04-24 06:32:09 +0900

    ユーザー登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :nickname
+    validates :email,
+      format: { with: %r{\A[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\z} }
+    validates :first_name,
+      format: { with: /\A([ぁ-んァ-ン一-龥々]|ー)+\z/ }
+    validates :last_name,
+      format: { with: /\A([ぁ-んァ-ン一-龥々]|ー)+\z/ }
+    validates :first_name_kana,
+      format: { with: /\A([ァ-ン]|ー)+\z/ }
+    validates :last_name_kana,
+      format: { with: /\A([ァ-ン]|ー)+\z/ }
+    validates :birth_day
+  end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+
+  has_many :games
+  has_many :comments
 end
